7635a5ca83f0509a7ddc6000d4bf935293b40c86
LeifMorgan <leif.e.morgan@gmail.com>
2015-09-02 10:44:52 -0700

    added first name to index

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,37 +14,67 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX = (int)(Math.random()*300);
+int startX = (int)(Math.random()*250);
 int startY = 0;
-int endX = (int)(Math.random()*300);
+int endX = (int)(Math.random()*250);
 int endY = 0;
+
 
 public void setup()
 {
   size(300,300);
   strokeWeight(1);
-  background(0,0,102);	
+  background(0);	
 
 }
 public void draw()
 {
-	
+	int color1 = (int)(Math.random()*50+50);
+	int color2 = (int)(Math.random()*50+50);
+	fill(0,0,0,15);
+	rect(0,0,300,300);
+
+		//lightning
 	while (endX < 300) 
 	{
-		stroke(204,204,0);
+		
+		strokeWeight(2);
+		fill(color1,color2, 0);
+		stroke(color1,color2,0);
 		endY = startY + (int)(Math.random()*10);
 		endX = startX + (int)(Math.random()*21-9);
 		line(startX,startY,endX,endY);
 		startX = endX;
 		startY = endY;
 	}
+
+//clouds
+	stroke(0);
+	noStroke();
+	fill(130);
+	ellipse(150,0, 300,100);
+	ellipse(0,0, 300,150);
+	ellipse(300,0, 300,100);
+
+	//house
+	fill(170,126,50,100);
+	rect(120,240,70,70);
+	fill(255,108,72,100);
+	triangle(100,240,155,200,210,240);
+	rect(175,200,5,30);
+
+
 }
-public void mousePressed()
+public void mouseMoved()
 {
+	
 	startX = (int)(Math.random()*300);
 	startY = 0;
 	endX = (int)(Math.random()*300);
 	endY = 0;
+
+
+	
 }
 
   static public void main(String[] passedArgs) {
